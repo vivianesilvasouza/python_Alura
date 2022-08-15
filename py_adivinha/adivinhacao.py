0a004a43a15e08a423c3302dd5d39a08192f57aa
Viviane <vivianedasilvasouza2020@gmail.com>
2022-08-15 15:14:14 -0300

    inicial

--- py_adivinha/adivinhacao.py
+++ py_adivinha/adivinhacao.py
@@ -2,16 +2,16 @@ print("**********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("**********************************")
 
-numero_secreto= 42
-chute_str= input("digite o seu número: ")
+numero_secreto = 42
+chute_str = input("digite o seu número: ")
 
 print('você digitou', chute_str)
 
-chute = int(chute_str)
+chute = int(chute_str)  # realizando a conversão de str para int
 
-if (numero_secreto == chute) :
+if (numero_secreto == chute):
     print('você acertou!')
 else:
     print('você errou')
 
-
+print('fim do jogo')
